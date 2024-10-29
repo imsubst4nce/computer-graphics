1667593b8f1e4fa1acddf3b04c8558655768f573
imsubst4nce <nickolis21@gmail.com>
2024-10-29 14:12:23 +0300

    fixed motion of char object

--- Ask1/Source-1A.cpp
+++ Ask1/Source-1A.cpp
@@ -384,7 +384,7 @@ int main(void)
     // setup maze
     glBindVertexArray(VAOs[0]);
     glBindBuffer(GL_ARRAY_BUFFER, mazevertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(maze_vertex_buffer_data), maze_vertex_buffer_data, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(maze_vertex_buffer_data), maze_vertex_buffer_data, GL_STATIC_DRAW); // GL_STATIC_DRAW makes buffer immutable
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[0]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(maze_indices), maze_indices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -393,6 +393,8 @@ int main(void)
     // setup moveable character
     glBindVertexArray(VAOs[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, charvertexbuffer);
+    // GL_DYNAMIC_DRAW makes the buffer mutable, 
+    // and since we move our character that means we need to make it dynamic
 	glBufferData(GL_ARRAY_BUFFER, sizeof(char_vertex_buffer_data), char_vertex_buffer_data, GL_DYNAMIC_DRAW);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOs[1]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(char_indices), char_indices, GL_DYNAMIC_DRAW);
